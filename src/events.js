bbf31c62aa35b23376b5652b7f1be7e2cfc5ae9d
Matthew <matthew.stewart827@gmail.com>
2023-08-06 13:34:53 -0500

    complete

---
+++ src/events.js
@@ -0,0 +1,13 @@
+// (async () => {
+//     const rawResponse = await fetch("http://localhost:3000/ramens", {
+//         method: "POST",
+//         headers: {
+//             "Accept": "application/json", 
+//             "Content-Type": "application/json"
+//         },
+//         body: JSON.stringify(ramens)
+//     })
+//     const content = await rawResponse.json()
+
+//     console.log(content)
+// })
--- src/index.js
+++ src/index.js
@@ -1,1 +1,55 @@
-// write your code here
+const baseURl = "http://localhost:3000/ramens"
+const ramenMenuDiv = document.querySelector('#ramen-menu')
+const ramenDetailDiv = document.querySelector('#ramen-detail')
+const newRamenForm = document.querySelector('#new-ramen')
+
+fetch(`${baseURl}`)
+.then(response => response.json())
+.then(ramen => {
+    ramen.forEach(renderRamen)
+})
+
+function renderRamen(ramen) {
+    let ramenImage = document.createElement('img')
+    ramenImage.src = ramen.image
+    ramenMenuDiv.append(ramenImage)
+    ramenImage.addEventListener('click', () => {
+        displayRamenDetails(ramen)
+    })
+}
+
+function displayRamenDetails(ramen) {
+    let ramenName = document.getElementById("detail-name")
+    let ramenRest = document.getElementById("detail-rest")
+    let ramenDetailImage = document.getElementById("detail-image")
+    let ramenRating = document.getElementById("rating-display")
+    let ramenComment = document.getElementById("comment-display")
+
+    ramenName.innerText = ramen.name
+    ramenRest.innerText = ramen.restaurant
+    ramenDetailImage.src = ramen.image
+    ramenRating.innerText = ramen.rating
+    ramenComment.innerText = ramen.comment
+}
+
+newRamenForm.addEventListener('submit', e => {
+    e.preventDefault()
+    let newRamenName = document.getElementById("new-name").value
+    let newRamenRestaurant = document.getElementById("new-restaurant").value
+    let newRamenImage = document.getElementById("new-image").value
+    let newRamenRating = document.getElementById("new-rating").value
+    let newRamenComment = document.getElementById("new-comment").value
+
+    const newRamen = {
+        "name": newRamenName,
+        "image": newRamenImage,
+        "restaurant": newRamenRestaurant,
+        "rating": newRamenRating,
+        "comment": newRamenComment
+    }
+
+    renderRamen(newRamen)
+
+})
+
+
